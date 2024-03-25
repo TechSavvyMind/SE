8726ac2f99466592bf618310c89d2a527dcd9210
Deepak Yerram <130681810+TechSavvyMind@users.noreply.github.com>
2024-03-25 14:42:44 +0530

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from tensorflow import keras
 from keras.models import load_model
-from keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 import snscrape.modules.twitter as sntwitter
 import datetime as dt
